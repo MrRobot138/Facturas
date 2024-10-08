75fa4d54d6a8d723a29f53c10d427382426a25c9
Juan Miguel Manrique <j.manriquec@uniandes.edu.co>
2024-10-08 15:04:10 -0500

    refactor

--- ofipensiones_estudiantes/ofipensiones_estudiantes/urls.py
+++ ofipensiones_estudiantes/ofipensiones_estudiantes/urls.py
@@ -1,4 +1,4 @@
-"""ofipenciones_estudiantes URL Configuration
+"""ofipensiones_estudiantes URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
--- ofipensiones_estudiantes/ofipensiones_estudiantes/wsgi.py
+++ ofipensiones_estudiantes/ofipensiones_estudiantes/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ofipenciones_estudiantes project.
+WSGI config for ofipensiones_estudiantes project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ofipenciones_estudiantes.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ofipensiones_estudiantes.settings')
 
 application = get_wsgi_application()
