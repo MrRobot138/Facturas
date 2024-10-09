930b6a35a4e9025f25729cea3f924e7c40b4c5f9
FranklinSRomero <113615053+FranklinSRomero@users.noreply.github.com>
2024-10-09 09:33:40 -0500

    add health Check

--- ofipensiones_estudiantes/estudiantes/urls.py
+++ ofipensiones_estudiantes/estudiantes/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('consulta/<int:id_estudiante>/', views.consulta_estado_pago, name='consulta_estado_pago'),
+    path('health-check/', views.healthCheck),  # Nueva línea añadida para Health Check     
 ]
 
--- ofipensiones_estudiantes/estudiantes/views.py
+++ ofipensiones_estudiantes/estudiantes/views.py
@@ -1,7 +1,11 @@
 #from django.shortcuts import render
 from django.http import JsonResponse
+from django.http import HttpResponse
 from .logic.logic_estudiante import get_recibos
 
 def consulta_estado_pago(request, id_estudiante):
     data = get_recibos(id_estudiante)
     return JsonResponse({'data': data})
+
+def healthCheck(request):  # Nueva función añadida para Health Check
+    return HttpResponse('ok')
