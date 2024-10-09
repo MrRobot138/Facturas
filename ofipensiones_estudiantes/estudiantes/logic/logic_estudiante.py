7c80f975514cfc156a340ce45c4b6406759f4fb1
Juan Miguel Manrique <j.manriquec@uniandes.edu.co>
2024-10-09 09:41:22 -0500

    consulta para todos los estudiantes

--- ofipensiones_estudiantes/estudiantes/logic/logic_estudiante.py
+++ ofipensiones_estudiantes/estudiantes/logic/logic_estudiante.py
@@ -9,3 +9,19 @@ def get_recibos(id_estudiante):
         'estadoCuenta': recibo.resumenDeCuenta.estado
     } for recibo in recibos] 
     return (data)
+
+def get_all_recibos():
+    recibos = ReciboDePago.objects.all()
+    data = {}
+
+    for recibo in recibos:
+        estudiante_id = recibo.resumenDeCuenta.estudiante.codigo
+        if estudiante_id not in data:
+            data[estudiante_id] = []
+        data[estudiante_id].append({
+            'idRecibo': recibo.idRecibo,
+            'fechaPago': recibo.fechaPago,
+            'valorCancelado': recibo.valorCancelado,
+            'estadoCuenta': recibo.resumenDeCuenta.estado
+        })
+    return (data)
