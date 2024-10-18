dfb177b0eff9d15d833e88c4561950130af5feef
juanmi30 <j.manriquec@uniandes.edu.co>
2024-10-18 14:29:01 -0500

    cambios para asr escalabilidad

--- ofipensiones_estudiantes/estudiantes/logic/logic_estudiante.py
+++ ofipensiones_estudiantes/estudiantes/logic/logic_estudiante.py
@@ -1,4 +1,5 @@
-from ..models import ReciboDePago
+from ..models import ReciboDePago, Estudiante
+from django.shortcuts import get_object_or_404
 
 def get_recibos(id_estudiante):
     recibos = ReciboDePago.objects.filter(resumenDeCuenta__estudiante_id=id_estudiante)
@@ -24,4 +25,18 @@ def get_all_recibos():
             'valorCancelado': recibo.valorCancelado,
             'estadoCuenta': recibo.resumenDeCuenta.estado
         })
+    return (data)
+
+def get_estudiantes():
+    estudiantes = Estudiante.objects.all().values('codigo', 'nombre', 'grado', 'institucion__nombre')
+    return (estudiantes)
+
+def get_estudiante(id_estudiante):
+    estudiante = get_object_or_404(Estudiante, id=id_estudiante)
+    data = {
+        'codigo': estudiante.codigo,
+        'nombre': estudiante.nombre,
+        'grado': estudiante.grado,
+        'institucion': estudiante.institucion.nombre if estudiante.institucion else None
+    }
     return (data)
--- ofipensiones_estudiantes/estudiantes/views.py
+++ ofipensiones_estudiantes/estudiantes/views.py
@@ -1,6 +1,6 @@
 #from django.shortcuts import render
 from django.http import JsonResponse
-from .logic.logic_estudiante import get_recibos, get_all_recibos
+from .logic.logic_estudiante import get_recibos, get_all_recibos, get_estudiantes, get_estudiante
 from django.http import HttpResponse
 
 def consulta_estado_pago(request, id_estudiante):
@@ -10,6 +10,14 @@ def consulta_estado_pago(request, id_estudiante):
 def consulta_all_estado_pago(request):
     data = get_all_recibos()
     return JsonResponse({'estudiantes': data})
+
+def consulta_estudiantes(request):
+    data = get_estudiantes()
+    return JsonResponse({'estudiantes': list(data)})
+
+def consulta_estudiante_por_id(request, id_estudiante):
+    data = get_estudiante(id_estudiante)
+    return JsonResponse({'estudiante': data})
 
 def healthCheck(request):  # Nueva función añadida para Health Check
     return HttpResponse('ok')
